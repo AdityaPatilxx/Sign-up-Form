538e23a3e31228d19f22e8ea911217cc6b1e1911
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2025-01-31 00:29:35 +0530

    Refactor login page layout and styles, add toggle functionality for Mars animation

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.querySelector("#form-section form");
-  
-    if (loginForm) {
-      loginForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        const email = loginForm.querySelector("#email").value;
-        const password = loginForm.querySelector("#password").value;
-  
-        // Example: Check if email and password are valid
-        if (email && password) {
-          alert("Login successful!");
-          // Redirect to another page or perform other actions
-        } else {
-          alert("Please fill in all fields.");
-        }
-      });
-    }
-  });
+  const loginForm = document.querySelector("#form-section form");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const email = loginForm.querySelector("#email").value;
+      const password = loginForm.querySelector("#password").value;
+
+      // Example: Check if email and password are valid
+      if (email && password) {
+        alert("Login successful!");
+        // Redirect to another page or perform other actions
+      } else {
+        alert("Please fill in all fields.");
+      }
+    });
+  }
+});
+
+
+function change() {
+  let mars = document.querySelector('.mars')
+  let headline = document.querySelector('.headline')
+
+  mars.classList.toggle('active')
+}
